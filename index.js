f60020d4f532ad9d4efd78f22d4e95b5a5e0f0ae
Leonardi Pérez <marcolpr98@gmail.com>
2020-04-26 14:33:55 -0500

    WIP

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-window.onload = function() {
+window.onload = function () {
   //Cookies
   window.cookieconsent.initialise({
     content: {
-      header: "Cookies used on the website!",
+      header: 'Se utilizan cookies!',
       message:
-        "This website uses cookies to ensure you get the best experience on it",
-      dismiss: "Got it!",
-      allow: "Allow cookies",
-      deny: "Decline",
-      link: "Learn about Cookies",
-      href: "http://cookiesandyou.com",
-      close: "&#x274c;",
+        'Este sitio web utiliza cookies para asegurarse que reciba la mejor experiencia',
+      dismiss: 'Ok',
+      allow: 'Permitir cookies',
+      deny: 'Rechazar',
+      link: 'Aprender mas de Cookies',
+      href: 'http://cookiesandyou.com',
+      close: '&#x274c;',
     },
     palette: {
       popup: {
-        background: "#000",
+        background: '#000',
       },
       button: {
-        background: "#f1d600",
+        background: '#f1d600',
       },
     },
-    theme: "classic",
+    theme: 'classic',
   });
 };
